1ee4d74fbfda6a58b28321e718fcb5a33df7c428
Aleksandra2806 <k.sasha9636@mail.com>
2022-07-10 15:19:03 +0300

    Git: Quick Commit изменения ДЗ 1,2,5 Git: Push

--- Доп.задание1_5.js
+++ Доп.задание1_5.js
@@ -1,6 +1,6 @@
 // Задание 5
 
-const input = document.querySelectorAll('input[type=text]');
+const input = document.querySelectorAll('.clearInput');
 const clearButton = document.getElementById('clearButton'); 
 clearButton.onclick = () => {
     for (let i = 0;  i < input.length; i++) {
